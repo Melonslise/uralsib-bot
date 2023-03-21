06992788ab21bfab9714673060aa425d1ebde0e1
Mercy <arturzartdinov@gmail.com>
2023-03-21 12:37:01 +0300

    Add inline keyboard

--- app.js
+++ app.js
@@ -8,28 +8,49 @@ const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
 console.log("Bot started");
 
-bot.on("message", msg =>
-{
-	const fromId = msg.from.id;
 
-	if (!msg.text)
+
+function execCmdByIdx(fromId, cmdIdx)
+{
+	if(cmdIdx < 0)
+	{
+		bot.sendMessage(fromId, "Неизвестная команда");
+
+		return;
+	}
+
+	const cmdInfo = Commands[cmdIdx];
+
+	if(cmdInfo.type === "text")
+	{
+		bot.sendMessage(fromId, cmdInfo.arg, { reply_markup: { inline_keyboard: cmdInfo.keyboard.map(e => e.map(t => ({ text: t, callback_data: Commands.findIndex(c => c.cmd === t.toLowerCase()) }))) } });
+	}
+}
+
+function execCmd(fromId, text)
+{
+	if (!text)
 	{
 		bot.sendMessage(fromId, "На данный момент бот поддерживает только текстовые команды");
 
 		return
 	}
 
-	const cmdInfo = Commands[msg.text.toLowerCase()];
+	const cmdIdx = Commands.findIndex(e => e.cmd === text.toLowerCase());
 
-	if(!!cmdInfo)
-	{
-		if(cmdInfo.type === "text")
-		{
-			bot.sendMessage(fromId, cmdInfo.arg, { reply_markup: { keyboard: cmdInfo.keyboard, one_time_keyboard: true } });
-		}
+	execCmdByIdx(fromId, cmdIdx);
+}
 
-		return;
-	}
 
-	bot.sendMessage(fromId, "Неизвестная команда");
+
+bot.on("message", msg =>
+{
+	execCmd(msg.from.id, msg.text);
 });
+
+bot.on("callback_query", query =>
+{
+	execCmdByIdx(query.from.id, +query.data);
+});
+
+bot.on("polling_error", console.log);
